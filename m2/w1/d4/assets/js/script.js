3e90cd3e9ccd9bdb8749c8a459c3dbbbd4518b7b
ewelinaczaja <ewelinaczaja23@gmail.com>
2022-10-27 12:05:33 +0200

    todolist

---
+++ m2/w1/d4/assets/js/script.js
@@ -0,0 +1,37 @@
+function grande() {
+    document.getElementById("h1").style.fontSize = "300%";
+  }
+  function colore() {
+    document.getElementById("h1").style.color = "red";
+  }
+  function maiuscolo() {
+    document.getElementById("h1").style.textTransform = "uppercase";
+  }
+  function nascondi() {
+    document.getElementById("h1").style.visibility = "hidden";
+  }
+  function mostrare() {
+    document.getElementById("h1").style.visibility = "visible";
+  }
+  
+  function cancella1() {
+    document.getElementById("lista1").classList.toggle("stileLista");
+ }
+ function cancella2() {
+    document.getElementById("lista2").classList.toggle("stileLista");
+ }
+ function cancella3() {
+    document.getElementById("lista3").classList.toggle("stileLista");
+ }
+ function cancella4() {
+    document.getElementById("lista4").classList.toggle("stileLista");
+ }
+
+ function apri() {
+    document.getElementById("menu").classList.toggle("newMenu");
+ }
+
+
+ 
+
+ 
